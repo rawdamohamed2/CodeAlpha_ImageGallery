c8bc9f2310301a52fc1fa5cb2262e47fe4584c79
Rawda  mohamed <rawdamohamed618@gmail.com>
2025-07-26 00:30:27 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,121 +1,159 @@
-let btnsfilter = document.querySelectorAll('.btnfilter');
+let btnsfilter = document.querySelectorAll('.filter');
 const searchInput = document.getElementById('searchInput');
 const btnsearch = document.getElementById('btnsearch');
 const lightBox = document.getElementById('lightBox');
-let btnPagenavigation = document.querySelectorAll('.btn-Pagenavigation');
+let btnPagenavigation = document.querySelectorAll('.btnPagenavigation');
 const lightboximg = document.getElementById('lightbox-img');
 const photographer = document.getElementById('photographer');
 const themetoggle = document.getElementById('theme-toggle');
-let oldfavourites = JSON.parse(localStorage.getItem('favourites')) || [];
-let currentQuery = 'curated';
+let currentquery = 'curated';
 let currentPage = 1;
-let currentFilter = 'all';
 let photos = [];
 let currentPhotoIndex = 0;
 let favourites = JSON.parse(localStorage.getItem('favourites')) || [];
 let favouritefilter = JSON.parse(localStorage.getItem('favouritefilter')) || [];
 const loader = document.getElementById('loader');
-async function fetchImages(query,page = 1,perPage = 20) {
+let totalPages;
 
-  const res = query === 'curated'
-    ? `https://api.pexels.com/v1/curated?page=${page}&per_page=${perPage}`
-    : `https://api.pexels.com/v1/search?query=${query}&page=${page}&per_page=${perPage}`;
-
+async function fetchImages(query,page) {
   try {
-      const response = await fetch(res, {
-        headers: {
-                Authorization: 'RT4KC88eJi9m7MD3RDmYA3iUVfVUlCu3GP7Au6r6gXzLNECIS20jZs2X'
-            }
+        const res = await fetch(`https://api.pexels.com/v1/search?page=${page?page:1}&per_page=12&query=${query?query:'curated'}`, {
+          headers: {
+            Authorization: 'RT4KC88eJi9m7MD3RDmYA3iUVfVUlCu3GP7Au6r6gXzLNECIS20jZs2X'
+          }
         });
-        const data = await response.json();
+        const data = await res.json();
         let images = await data.photos;
-        photos=images;
+        if (!totalPages) {
+          totalPages = Math.ceil(data.total_results / data.per_page);
+        }
+        photos= images;
         console.log(photos);
-        localStorage.setItem('pexelsPhotos', JSON.stringify(images)); 
+        displayImages(images);
         currentPage=page;
-        currentQuery=query || 'curated'
-        loader.classList.add('d-none');;
-        displayImages(images);      
+        currentquery=query;
+        console.log(currentquery,currentPage);
+        loader.classList.add('d-none');
       } catch (err) {
         console.error('Error:', err);
+        loader.classList.add('d-none');
       }
 }
-fetchImages();
 
-function displayImages(photos){
+fetchImages(currentquery, currentPage);
+
+function displayImages(photos) {
+  if (!photos || photos.length === 0) {
+    document.getElementById('galleryBox').innerHTML = '<p>No images found</p>';
+    return;
+  }
+
   let box = '';
-  for(let i=0;i<photos.length;i++){
-     const isFavourite = favourites.some(fav => fav.id === photos[i].id);
-      box += `<div class="masonry-item  position-relative rounded-4 overflow-hidden hovercolimage">
-                          <img src="${photos[i].src.original}" alt="${photos[i].alt || 'Image from Pexels'}" data-full="${photos[i].src.original}" data-index="${i}" loading="lazy" class="w-100 rounded-4 hoverimg d-block">
-                          <p class="fontfamily text-white position-absolute name rounded-2">Photo by: 
-                            <a href="${photos[i].photographer_url}" class="fontfamily hoverfontcolor" target="_blank">
-                              ${photos[i].photographer}
-                            </a>
-                          </p>
-                         <button type="button" class="btn position-absolute favouritebtn" onclick="addToFavourites(${i}, this)">
-                          <i class="${isFavourite ? 'fa-solid favouriteiconhover' : 'fa-regular favouriteicon'} fa-heart "></i>
-                        </button>
-                      </div>`;
+  photos.forEach((photo, i) => {
+    const isFavourite = favourites.some(fav => fav.id === photo.id);
+    box += `<div class="masonry-item position-relative rounded-4 overflow-hidden hovercolimage">
+              <img src="${photo.src.large}" alt="${photo.alt || 'Image from Pexels'}" 
+                   data-full="${photo.src.original}" data-index="${i}" loading="lazy" 
+                   class="w-100 rounded-4 hoverimg d-block" data-bs-toggle="modal" 
+                   data-bs-target="#exampleModal">
+              <p class="fontfamily text-white position-absolute name rounded-2">Photo by: 
+                <a href="${photo.photographer_url}" class="fontfamily hoverfontcolor" target="_blank">
+                  ${photo.photographer}
+                </a>
+              </p>
+              <button type="button" class="btn position-absolute favouritebtn" onclick="addToFavourites(${i}, this)">
+                <i class="${isFavourite ? 'fa-solid favouriteiconhover' : 'fa-regular favouriteicon'} fa-heart"></i>
+              </button>
+            </div>`;
+  });
 
-    }
-    document.getElementById('galleryBox').innerHTML = box;
+  document.getElementById('galleryBox').innerHTML = box;
 
-    document.querySelectorAll('.masonry-item img').forEach(img => {
-        img.addEventListener('click', () => {
-            currentPhotoIndex = img.dataset.index;
-            openLightbox();
-        });
+  document.querySelectorAll('.masonry-item img').forEach(img => {
+    img.addEventListener('click', () => {
+      currentPhotoIndex = parseInt(img.dataset.index);
+      displayImagemodel();
     });
+  });
 }
 
 function addToFavourites(index ,btnElement) {
   const photo = photos[index];
-  if (favourites.some(fav => fav.id === photo.id)) {
-    alert('Image already added to favourites!');
-    return;
+  const favIndex = favourites.findIndex(fav => fav.id === photo.id);
+  if (favIndex !== -1) {
+    favourites.splice(favIndex, 1);
+    favouritefilter.splice(favIndex, 1);
+    localStorage.setItem('favouritefilter', JSON.stringify(favouritefilter));
+    localStorage.setItem('favourites', JSON.stringify(favourites));
+    btnElement.innerHTML = `<i class="fa-regular fa-heart favouriteicon"></i>`;
+    showTempMessage('romoved from favourites');
   }
   else{
   favourites.push(photo);
-  favouritefilter.push(currentQuery);
+  favouritefilter.push(currentquery);
   localStorage.setItem('favouritefilter', JSON.stringify(favouritefilter));
-  console.log(favouritefilter);
   localStorage.setItem('favourites', JSON.stringify(favourites));
-  console.log(favourites);
   btnElement.innerHTML = `<i class="fa-solid fa-heart favouriteiconhover"></i>`;
-  // alert('Image added to favourites!');
+  showTempMessage('Added to favourites');
 }
 }
 
-function openLightbox() {
+function showTempMessage(text) {
+  const msg = document.createElement('div');
+  msg.textContent = text;
+  msg.className = 'position-fixed top-0 start-50 translate-middle-x bg-dark text-white px-3 py-2 rounded-3 z-3';
+  document.body.appendChild(msg);
+  setTimeout(() => msg.remove(), 2000);
+}
+
+
+function displayImagemodel() {
   const photo = photos[currentPhotoIndex];
-  lightBox.classList.add('d-block');
-  lightBox.classList.remove('d-none');
-  lightboximg.src = photo.src.original;
+  lightboximg.src = photo.src.large || photo.src.medium;
   photographer.innerHTML = `Photo by: <a href="${photo.photographer_url}" class="hoverfontcolor" target="_blank">${photo.photographer}</a>`;
-}
-function closeLightbox(){
-  lightBox.classList.add('d-none');
-  lightBox.classList.remove('d-block');
-}
-function previousLightbox(){
-  currentPhotoIndex--;
-  openLightbox(currentPhotoIndex);
 
+  const nextIndex = (currentPhotoIndex + 1) % photos.length;
+  const prevIndex = (currentPhotoIndex - 1 + photos.length) % photos.length;
+
+  new Image().src = photos[nextIndex].src.medium;
+  new Image().src = photos[prevIndex].src.medium;
 }
-function nextLightbox(){
-  currentPhotoIndex++;
-  openLightbox(currentPhotoIndex); 
+
+function previousLightbox() {
+  if (!photos) {return;}
+  else if (currentPhotoIndex === 0) {
+    currentPhotoIndex = photos.length - 1;
+    displayImagemodel();
+  }
+  else{
+    currentPhotoIndex --;
+    displayImagemodel();
+  }
+  
 }
+
+function nextLightbox() {
+  if (!photos) return;
+  else if (currentPhotoIndex === photos.length - 1) {
+    currentPhotoIndex = 0;
+    displayImagemodel();
+  }
+  else{
+    currentPhotoIndex++;
+    displayImagemodel();
+  }
+  
+}
+
 
 btnsfilter.forEach(btn => {
   btn.addEventListener('click', () => {
     loader.classList.remove('d-none');
     const filter = btn.dataset.filter;
-    if (filter !== currentFilter) {
+    if (filter !== currentquery) {
       btnsfilter.forEach(otherBtn => otherBtn.classList.remove('active'));
       btn.classList.add('active');
-      currentFilter = filter;
+      currentquery = filter;
       currentPage = 1;
       fetchImages(filter, currentPage);
     }
@@ -127,31 +165,60 @@ btnsearch.addEventListener('click', () => {
   loader.classList.remove('d-none');
   const query = searchInput.value;
   if (query) {
-    fetchImages(query);
+    fetchImages(query,1);
   }
   else{
-    fetchImages('curated');
+    fetchImages('curated',1);
   }
   searchInput.value="";
 });
 
+
+
 btnPagenavigation.forEach(btn => {
   btn.addEventListener('click', () => {
-    
     const page = btn.dataset.page;
-    if (page==="Previous" && currentPage > 1) {
-      currentPage--;
-      fetchImages(currentFilter, currentPage);
+    if (page === "Previous") {
       loader.classList.remove('d-none');
-    } else if (page==="Next") {
-      currentPage++;
-      fetchImages(currentFilter, currentPage);
+      loader.classList.add('heightleaderdownload')
+      loader.classList.remove('heightleader');
+      previousPage();
+    } else if (page === "Next") {
       loader.classList.remove('d-none');
+      loader.classList.add('heightleaderdownload')
+      loader.classList.remove('heightleader');
+      nextPage();
     }
   });
 });
 
+function nextPage() {
+
+  if (currentPage < totalPages) {
+    currentPage++;
+    fetchImages(currentquery, currentPage);
+  } else {
+    loader.classList.add('d-none');
+    showTempMessage('You reached the last page');
+    loader.classList.add('heightleader')
+    loader.classList.remove('heightleaderdownload');
+  }
+ 
+}
+
+function previousPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    fetchImages(currentquery, currentPage);
+  } else {
+    showTempMessage('Already on the first page');
+    loader.classList.add('d-none');
+    loader.classList.add('heightleader')
+    loader.classList.remove('heightleaderdownload');
+  }
+}
+
 themetoggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    themetoggle.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
+  document.body.classList.toggle('dark-mode');
+  themetoggle.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
 });
